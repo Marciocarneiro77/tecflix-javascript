949b82ec82698f3b375236605e8b13118d916c2a
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2021-04-06 21:00:03 -0300

    atualização site tecflix

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,13 +1,20 @@
-let filmes = ["Soul" , "A voz do silêncio" , "Simplesmente acontece"];
-console.log(filmes);
-console.log(filmes.length);
+let filmes = ["Batman v Superman.jpg" , "LigaJustiça.jpg" , "Cavaleiro-das-trevas.jpg","batmankeaton.jpg"];
+//console.log(filmes);
+//console.log(filmes.length);
 
 //console.log(filmes[0]);
 //console.log(filmes[1]);
 //console.log(filmes[2]);
 
-let quantidadeFilmes = 3;
-while(quantidadeFilmes > 0){
-    console.log(filmes[quantidadeFilmes]);
-   quantidadeFilmes --;
+//let quantidadeFilmes = 3;
+//while(quantidadeFilmes > 0){
+   // console.log(filmes[quantidadeFilmes]);
+   //quantidadeFilmes --;
+//}
+//filmes.push("liga da Justiça");
+
+
+for(let i = 0; i < filmes.length; i++){
+    document.write("<img src='img/" + filmes[i] +"'>");
+
 }
